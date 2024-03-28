3886ff1fe695106993f7b73f4892d2469042eb11
SergMo <msbox16@gmail.com>
2024-03-28 14:57:17 +0000

    forms

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+const SigninForm = () => {
+  return (
+    <div>
+      <Button>Click me too</Button>
+    </div>
+  )
+}
+
+export default SigninForm
